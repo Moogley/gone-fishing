a4bd5d7f5977045cc0d04d49a0d20bddcc0af0c5
Shane Mosley <shanemosley14@gmail.com>
2020-09-20 19:25:26 -0400

    Updates

---
+++ precache-manifest.9b432a3fce97c4c4c4a6781765a12695.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238ce96697c8bcab09a0c835409d3a5e",
+    "url": "/gone-fishing/index.html"
+  },
+  {
+    "revision": "f911e2e6294cb4063483",
+    "url": "/gone-fishing/static/css/main.484757ca.chunk.css"
+  },
+  {
+    "revision": "931ef9bf071001522cf8",
+    "url": "/gone-fishing/static/js/2.e3c1e765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gone-fishing/static/js/2.e3c1e765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f911e2e6294cb4063483",
+    "url": "/gone-fishing/static/js/main.bbe25528.chunk.js"
+  },
+  {
+    "revision": "1db4c92238db26197900",
+    "url": "/gone-fishing/static/js/runtime-main.dcbb61ce.js"
+  },
+  {
+    "revision": "dd06db962ed43a2030cfb7a2123c574c",
+    "url": "/gone-fishing/static/media/GFDock.dd06db96.png"
+  }
+]);
