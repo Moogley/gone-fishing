d9122139cd8cddccd1d736a9ddc4b4548686df4f
Shane Mosley <shanemosley14@gmail.com>
2020-09-20 14:58:32 -0400

    added viewport img, beginning to add time management for bite event

--- src/components/Fishing.js
+++ src/components/Fishing.js
@@ -1,25 +1,50 @@
 import React, { useState } from 'react'
 import './FishingStyle.css'
 
+let hooked = false
+
+const randomTime = (min, max) => {
+    const time = Math.floor(Math.random()*(max-min+1)+min);
+    console.log(`Fish will bite in ${time/1000} seconds.`)
+    return time
+}
+
+// const fishHooked = () => {
+    
+    
+// }
 
 const cast = () => {
     console.log("Casting line...")
-    setTimeout(() => {console.log("Bite!")}, 7000)
+    setTimeout(() => {
+        console.log(`You have a bite!`)
+        let interval = setInterval(() => {
+            console.log(`fishHooked - hooked = ${hooked}`)
+            hooked = true;
+            setTimeout(() => {
+                hooked = false;
+                clearInterval(interval)
+            }, 3000)
+        }, 1)
+    }, randomTime(7000, 9000))
 }
 
 export default function Fishing() {
-
     const [fish, catchFish] = useState(0);
+    console.log(hooked)    
 
     return (
         <div className="Fishing-Container">
-            <div className="Fishing-Viewport" onClick={() => catchFish(fish + 1)}>
-                <p>&gt;&lt;&gt; = {fish}</p>
-            </div>
+            <div className="Fishing-Viewport" onClick={() => catchFish(fish + 1)} />
             <div className="Fishing-Dashboard" >
-                <p>Caught: {fish}</p>
-                <button onClick={() => cast()}>Cast</button>
-                <button>Catch</button>
+                <p>Caught: {fish} 🐟</p>
+                <button onClick={cast}>Cast</button>
+                {hooked ? 
+                    <button onClick={() => catchFish(fish + 1)}>Catch</button> 
+                    : 
+                    <button onClick={() => console.log("Fish is not hooked")}>Catch</button>
+                }
+                
 
             </div>
         </div>
